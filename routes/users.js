2ce4f206161a98f8adf255da2a54279c23e45757
Pekka Alaluukas <Pekka.Alaluukas@mymail.com>
2022-02-17 13:17:31 +0200

    uusi versio

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Uusi versio');
+  res.send('Uusi versio user');
 });
 
 module.exports = router;
