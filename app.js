82cd06510e4c82560ddc00a83b77409e63d2aa53
Pekka Alaluukas <Pekka.Alaluukas@mymail.com>
2022-02-24 18:40:56 +0200

    autentikointi tietokannan avulla

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 const basicAuth = require('express-basic-auth');
 const dotenv = require('dotenv');
 
+//jos autentikointi tehdään tietokannasta tarvitaan seuraavat 2 riviä
+const db = require('./database');
+const bcrypt = require('bcryptjs');
+//autentikointi 
+
 var bookRouter = require('./routes/book');
 var studentRouter = require('./routes/student');
 
@@ -15,6 +20,37 @@ app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
 
 dotenv.config();
 
+function myAuthorizer(username, password,cb){
+    db.query('SELECT password FROM user_table WHERE username = ?',[username], 
+      function(dbError, dbResults, fields) {
+        if(dbError){
+              response.json(dbError);
+            }
+        else {
+          if (dbResults.length > 0) {
+            bcrypt.compare(password,dbResults[0].password, 
+              function(err,res) {
+                if(res) {
+                  console.log("succes");
+                  return cb(null, true);
+                }
+                else {
+                  console.log("wrong password");
+                  return cb(null, false);
+                }			
+                response.end();
+              }
+            );
+          }
+          else{
+            console.log("user does not exists");
+            return cb(null, false);
+          }
+        }
+      }
+    );
+  }
+/*
 function myAuthorizer(username, password, cb){
     if(username===process.env.authUser && password ===process.env.authPassword){
         return cb(null, true);
@@ -22,7 +58,7 @@ function myAuthorizer(username, password, cb){
     else{
         return cb(null, false);
     }
-}
+}*/
 
 
 app.use(logger('dev'));
