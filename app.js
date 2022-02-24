8d2a59b2812b241a4395ef7279763a5c251086d4
Pekka Alaluukas <Pekka.Alaluukas@mymail.com>
2022-02-24 12:46:53 +0200

    koe

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
+var studentRouter = require('./routes/student');
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/student', studentRouter);
 
 module.exports = app;
